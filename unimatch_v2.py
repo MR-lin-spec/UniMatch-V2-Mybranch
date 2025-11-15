2ff5664f16aee3284b12ddef8eb26eba683b628f
Linzihan <3304452595@qq.com>
2025-11-15 22:54:16 +0800

    云端成功跑通版本的结果

--- unimatch_v2.py
+++ unimatch_v2.py
@@ -111,12 +111,12 @@ def main():
     
     trainsampler_l = torch.utils.data.distributed.DistributedSampler(trainset_l)
     trainloader_l = DataLoader(
-        trainset_l, batch_size=cfg['batch_size'], pin_memory=True, num_workers=4, drop_last=True, sampler=trainsampler_l
+        trainset_l, batch_size=cfg['batch_size'], pin_memory=True, num_workers=10, drop_last=True, sampler=trainsampler_l
     )
     
     trainsampler_u = torch.utils.data.distributed.DistributedSampler(trainset_u)
     trainloader_u = DataLoader(
-        trainset_u, batch_size=cfg['batch_size'], pin_memory=True, num_workers=4, drop_last=True, sampler=trainsampler_u
+        trainset_u, batch_size=cfg['batch_size'], pin_memory=True, num_workers=10, drop_last=True, sampler=trainsampler_u
     )
     
     valsampler = torch.utils.data.distributed.DistributedSampler(valset)
